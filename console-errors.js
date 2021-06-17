32392a5dbdcb56d79e5164820c7fcc09ac1d8bd8
Abbu Tulu <atullu1234@gmail.com>
2021-06-17 01:32:45 -0400

    actual first commit

---
+++ console-errors.js
@@ -0,0 +1,9 @@
+function changeBackgroundColor(element, color) {
+  element.style.backgroundColor = color;
+}
+
+const myBox = document.getElementById("myBox");
+
+changeBackgroundColor(myBox, "blue");
+
+prompt("What is your name");
